255ffdc29fae47dc2ac3e458158ebc959c971bb9
Erik Sabino <eriksabino@Eriks-MacBook-Air.local>
2025-03-28 21:40:13 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Olá, meu nome é Cebolinha. Digite uma flase pala eu lepetila: ");
+string frase = Console.ReadLine()!;
+frase = frase.Replace("r", "l").Replace("R", "L");
+Console.WriteLine($"Eu lepetilei: {frase}");
